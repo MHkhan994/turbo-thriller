2e86461690f11080993f54d59f3225fa356eea04
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-05-18 18:34:45 +0600

    banner

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, NavNavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -11,26 +11,26 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="mt-6 text-black menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-md w-52">
+                        <ul tabIndex={0} className="mt-6 text-black menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52">
                             <li><NavLink to='/'>Home</NavLink></li>
-                            <li><NavLink to='/'>All Toys</NavLink></li>
-                            <li><NavLink to='/'>My Toys</NavLink></li>
-                            <li><NavLink to='/'>Add a Toy</NavLink></li>
-                            <li><NavLink to='/'>Blogs</NavLink></li>
+                            <li><NavLink to='/allToys'>All Toys</NavLink></li>
+                            <li><NavLink to='/myToys'>My Toys</NavLink></li>
+                            <li><NavLink to='/addToy'>Add a Toy</NavLink></li>
+                            <li><NavLink to='/blogs'>Blogs</NavLink></li>
                         </ul>
                     </div>
-                    <img src="logo.png" className='w-14 lg:w-20' alt="" />
+                    <img src="logo.png" className='w-10 lg:w-20' alt="" />
                     <button>
-                        <NavLink className='text-xl lg:text-3xl font-bold italic' to='/'>TurboThriller</NavLink>
+                        <NavLink className='text-lg lg:text-3xl font-bold italic' to='/'>TurboThriller</NavLink>
                     </button>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-2 text-lg">
+                    <ul className="menu menu-horizontal px-2 text-lg space-x-2">
                         <li><NavLink to='/'>Home</NavLink></li>
-                        <li><NavLink to='/'>All Toys</NavLink></li>
-                        <li><NavLink to='/'>My Toys</NavLink></li>
-                        <li><NavLink to='/'>Add a Toy</NavLink></li>
-                        <li><NavLink to='/'>Blogs</NavLink></li>
+                        <li><NavLink to='/allToys'>All Toys</NavLink></li>
+                        <li><NavLink to='/myToys'>My Toys</NavLink></li>
+                        <li><NavLink to='/addToy'>Add a Toy</NavLink></li>
+                        <li><NavLink to='/blogs'>Blogs</NavLink></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Banner = () => {
     return (
-        <div className='lg:h-[95vh]'>
-            <img className='w-full h-full' src="banner.png" alt="" />
+        <div className='h-[95vh]'>
+            <img className='lg:w-full h-full object-cover' src="banner.png" alt="" />
+            <div className='absolute lg:top-1/2 top-[43%] lg:left-[9%] left-[4%] text-white lg:w-1/2 w-3/4 space-y-5'>
+                <h1 className='lg:text-5xl text-3xl pe-7 font-semibold'>Play, Create, and Let Your Dreams Take Flight!</h1>
+                <button className='py-2'>
+                    <Link className='btn-primary'>See all Toys</Link>
+                </button>
+            </div>
         </div>
     );
 };
